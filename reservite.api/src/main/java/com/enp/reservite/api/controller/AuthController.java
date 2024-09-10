c372097e49171cccbbbba15790fed452971494e5
eneperezv <eneperezv@gmail.com>
2024-09-10 16:27:39 -0500

    Se incluye endpoint para autenticacion de usuarios y entidad Token.
     Changes to be committed:
    	new file:   reservite.api/src/main/java/com/enp/reservite/api/controller/AuthController.java
    	new file:   reservite.api/src/main/java/com/enp/reservite/api/entity/Token.java

---
+++ reservite.api/src/main/java/com/enp/reservite/api/controller/AuthController.java
@@ -0,0 +1,65 @@
+package com.enp.reservite.api.controller;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.enp.reservite.api.entity.ErrorDetails;
+import com.enp.reservite.api.entity.Token;
+import com.enp.reservite.api.entity.UserMyDetails;
+import com.enp.reservite.api.security.JwtService;
+import com.enp.reservite.api.security.LoginForm;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RestController
+@RequestMapping("/api/v1/reservite")
+public class AuthController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
+	
+	@Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtService jwtService;
+    @Autowired
+    private UserMyDetails userDetailService;
+	
+	@PostMapping("/auth")
+	public String authenticateAndGetToken(@RequestBody LoginForm loginForm) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginForm.username(), loginForm.password()
+        ));
+        try {
+        	if (authentication.isAuthenticated()) {
+                return convertObjectToJson(new Token(jwtService.generateToken(userDetailService.loadUserByUsername(loginForm.username()))));
+            } else {
+            	return convertObjectToJson(new ErrorDetails(new Date(),HttpStatus.UNAUTHORIZED.toString(),"Credenciales no autorizadas"));
+                //throw new UsernameNotFoundException("Invalid credentials");
+            }
+        }catch(Exception e) {
+        	System.out.println("ERROR-->"+e.getMessage()+"<--");
+        }
+		return null;
+    }
+	
+	private String convertObjectToJson(Object object) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            return "{\"error\": \"Error al convertir a JSON\"}";
+        }
+    }
+
+}
---
+++ reservite.api/src/main/java/com/enp/reservite/api/entity/Token.java
@@ -0,0 +1,25 @@
+package com.enp.reservite.api.entity;
+
+public class Token {
+	
+	private String token;
+
+	public Token(String token) {
+		super();
+		this.token = token;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	@Override
+	public String toString() {
+		return "Token [token=" + token + "]";
+	}
+
+}
