8fff0eb8c349959c1854d4f140498c84396052e4
eneperezv <eneperezv@gmail.com>
2024-09-10 16:12:54 -0500

    Se finaliza configuracion inicial de seguridad JWT y se incluyen entidades para autenticacion.
     Changes to be committed:
    	new file:   reservite.api/logs/reservite.api.log
    	modified:   reservite.api/pom.xml
    	modified:   reservite.api/src/main/java/com/enp/reservite/api/entity/UserMyDetails.java
    	modified:   reservite.api/src/main/java/com/enp/reservite/api/security/JwtAuthenticationFilter.java
    	modified:   reservite.api/src/main/java/com/enp/reservite/api/security/JwtService.java
    	modified:   reservite.api/src/main/resources/application.properties
    	new file:   reservite.api/src/test/java/com/enp/reservite/reservite/api/JwtSecretMaker.java
    
     Changes not staged for commit:
    	modified:   reservite.api/logs/reservite.api.log

--- reservite.api/src/main/java/com/enp/reservite/api/security/JwtService.java
+++ reservite.api/src/main/java/com/enp/reservite/api/security/JwtService.java
@@ -1,5 +1,62 @@
 package com.enp.reservite.api.security;
 
+import java.time.Instant;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Service
 public class JwtService {
+	
+	//KEY FOR AUTHENTICATION
+	//Use generateSecretKey method from de test folder to generate a new key
+	private static final String API_ACCESS = "C43A90EDF68A853BE97FA0890D14961E2D078D8F7AD7F0F3D01EA252C195455F3C612E2CD11F230DB171B732B6BE455E1F480C00D775B069E0A3EEC97EB9526A";
+	private static final long VALIDITY = TimeUnit.MINUTES.toMillis(60);
+	
+	public String generateToken(UserDetails userDetails) {
+		Map<String,String> claims = new HashMap<>();
+		claims.put("iss","bookmaster.api");
+		return Jwts.builder()
+			.claims(claims)
+			.subject(userDetails.getUsername())
+			.issuedAt(Date.from(Instant.now()))
+			.expiration(Date.from(Instant.now().plusMillis(VALIDITY)))
+			.signWith(generateKey())
+			.compact();
+	}
+	
+	private SecretKey generateKey() {
+		byte[] decodedKey = Base64.getDecoder().decode(API_ACCESS);
+		return Keys.hmacShaKeyFor(decodedKey);
+	}
+	
+	public String extractUsername(String jwt) {
+        Claims claims = getClaims(jwt);
+        return claims.getSubject();
+    }
+
+    private Claims getClaims(String jwt) {
+        return Jwts.parser()
+                 .verifyWith(generateKey())
+                 .build()
+                 .parseSignedClaims(jwt)
+                 .getPayload();
+    }
+
+    public boolean isTokenValid(String jwt) {
+        Claims claims = getClaims(jwt);
+        return claims.getExpiration().after(Date.from(Instant.now()));
+    }
 
 }
