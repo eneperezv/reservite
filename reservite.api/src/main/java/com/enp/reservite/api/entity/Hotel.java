f051fbc738fb267cb3708d989fba3c9180ad594c
eneperezv <eneperezv@gmail.com>
2024-09-11 22:11:31 -0500

    Se incluyen entidades para control de Hotel y Room.
     Changes to be committed:
    	new file:   reservite.api/src/main/java/com/enp/reservite/api/entity/Hotel.java
    	new file:   reservite.api/src/main/java/com/enp/reservite/api/entity/Room.java

---
+++ reservite.api/src/main/java/com/enp/reservite/api/entity/Hotel.java
@@ -0,0 +1,91 @@
+package com.enp.reservite.api.entity;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="dbo_hotel")
+public class Hotel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name="id_hotel", unique=true, nullable=false)
+	private Long id;
+	
+	@Column(name="name", length = 100)
+	private String name;
+	
+	@Column(name="address", length = 500)
+	private String address;
+	
+	@Column(name="phone", length = 50)
+	private String phone;
+	
+	@Column(name="email", length = 100)
+	private String email;
+	
+	public Hotel() {
+		
+	}
+
+	public Hotel(String name, String address, String phone, String email) {
+		super();
+		this.name = name;
+		this.address = address;
+		this.phone = phone;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Hotel [id=" + id + ", name=" + name + ", address=" + address + ", phone=" + phone + ", email=" + email
+				+ "]";
+	}
+
+}
---
+++ reservite.api/src/main/java/com/enp/reservite/api/entity/Room.java
@@ -0,0 +1,110 @@
+package com.enp.reservite.api.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="dbo_room")
+public class Room {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name="id_room", unique=true, nullable=false)
+	private Long id;
+	
+	@JoinColumn(name = "hotel", nullable = false)
+	@ManyToOne(targetEntity=Hotel.class, fetch=FetchType.LAZY)
+	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+	private Hotel hotel;
+	
+	@Column(name="floor")
+	private Long floor;
+	
+	@Column(name="number")
+	private Long number;
+	
+	@Column(name="capacity")
+	private Long capacity;
+	
+	@Column(name="description", length = 1500)
+	private String description;
+	
+	public Room() {
+		
+	}
+
+	public Room(Hotel hotel, Long floor, Long number, Long capacity, String description) {
+		super();
+		this.hotel = hotel;
+		this.floor = floor;
+		this.number = number;
+		this.capacity = capacity;
+		this.description = description;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Hotel getHotel() {
+		return hotel;
+	}
+
+	public void setHotel(Hotel hotel) {
+		this.hotel = hotel;
+	}
+
+	public Long getFloor() {
+		return floor;
+	}
+
+	public void setFloor(Long floor) {
+		this.floor = floor;
+	}
+
+	public Long getNumber() {
+		return number;
+	}
+
+	public void setNumber(Long number) {
+		this.number = number;
+	}
+
+	public Long getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(Long capacity) {
+		this.capacity = capacity;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "Room [id=" + id + ", hotel=" + hotel + ", floor=" + floor + ", number=" + number + ", capacity="
+				+ capacity + ", description=" + description + "]";
+	}
+
+}
