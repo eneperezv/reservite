49420f85d82b6605c275075298559d3d1ce33044
eneperezv <eneperezv@gmail.com>
2024-09-11 21:24:37 -0500

    Se incluye clase Controller para gestion de usuarios.
     Changes to be committed:
    	new file:   reservite.api/src/main/java/com/enp/reservite/api/controller/UserController.java
    	new file:   reservite.api/src/main/java/com/enp/reservite/api/service/UserService.java

---
+++ reservite.api/src/main/java/com/enp/reservite/api/controller/UserController.java
@@ -0,0 +1,81 @@
+package com.enp.reservite.api.controller;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.enp.reservite.api.entity.ErrorDetails;
+import com.enp.reservite.api.entity.User;
+import com.enp.reservite.api.service.UserService;
+
+@RestController
+@RequestMapping("/api/v1/reservite")
+public class UserController {
+	
+	@Autowired
+	UserService userService;
+	
+	@GetMapping("/user/{usuario}")
+	public ResponseEntity<?> getUsuarioByName(@PathVariable("usuario") String usuario){
+		User result;
+		try{
+			result = userService.findByUsuario(usuario);
+			if(result == null) {
+				ErrorDetails err = new ErrorDetails(new Date(),HttpStatus.NOT_FOUND.toString(),"Usuario <"+usuario+"> no existe");
+				return new ResponseEntity<ErrorDetails>(err,HttpStatus.NOT_FOUND);
+			}
+			return new ResponseEntity<User>(result, HttpStatus.OK);
+		}catch(Exception e){
+			ErrorDetails err = new ErrorDetails(new Date(),HttpStatus.NO_CONTENT.toString(),"INTERNAL SERVER ERROR");
+			return new ResponseEntity<ErrorDetails>(err, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@PostMapping("/user/create")
+	public ResponseEntity<?> createUsuario(@RequestBody User user){
+		User savedUser;
+		try{
+			savedUser = userService.save(user);
+			if(savedUser == null) {
+				ErrorDetails err = new ErrorDetails(new Date(),HttpStatus.NOT_FOUND.toString(),"Usuario <"+user+"> no existe");
+				return new ResponseEntity<ErrorDetails>(err,HttpStatus.NOT_FOUND);
+			}
+			return new ResponseEntity<User>(savedUser, HttpStatus.CREATED);
+		}catch(Exception e){
+			ErrorDetails err = new ErrorDetails(new Date(),HttpStatus.INTERNAL_SERVER_ERROR.toString(),"INTERNAL SERVER ERROR");
+			return new ResponseEntity<ErrorDetails>(err, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@PutMapping("/user/update")
+	public ResponseEntity<?> updateUsuario(@RequestBody User user) {
+	    try {
+	    	User result = userService.findByUsuario(user.getUsername());
+	        if (result == null) {
+	            ErrorDetails err = new ErrorDetails(new Date(), HttpStatus.NOT_FOUND.toString(), "Usuario <" + user.getUsername() + "> no existe");
+	            return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+	        }
+	        result.setPassword(user.getPassword());
+
+	        User savedUser = userService.save(user);
+	        if(savedUser == null) {
+				ErrorDetails err = new ErrorDetails(new Date(),HttpStatus.NOT_FOUND.toString(),"Usuario <"+user+"> no existe");
+				return new ResponseEntity<ErrorDetails>(err,HttpStatus.NOT_FOUND);
+			}
+			return new ResponseEntity<User>(savedUser, HttpStatus.OK);
+	    } catch (Exception e) {
+	        ErrorDetails err = new ErrorDetails(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), "INTERNAL SERVER ERROR");
+	        return new ResponseEntity<>(err, HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
+	}
+
+}
---
+++ reservite.api/src/main/java/com/enp/reservite/api/service/UserService.java
@@ -0,0 +1,17 @@
+package com.enp.reservite.api.service;
+
+import com.enp.reservite.api.entity.User;
+
+public class UserService {
+
+	public User findByUsuario(String usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public User save(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
