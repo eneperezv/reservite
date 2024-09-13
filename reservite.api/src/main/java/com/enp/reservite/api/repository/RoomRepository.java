373ad2624e6b47eec4ef1bb575be40d03f930266
eneperezv <eneperezv@gmail.com>
2024-09-12 22:21:11 -0500

    Se incluye implementacion de RoomService y RoomRepository.
     Changes to be committed:
    	modified:   reservite.api/src/main/java/com/enp/reservite/api/repository/RoomRepository.java
    	modified:   reservite.api/src/main/java/com/enp/reservite/api/service/RoomService.java

--- reservite.api/src/main/java/com/enp/reservite/api/repository/RoomRepository.java
+++ reservite.api/src/main/java/com/enp/reservite/api/repository/RoomRepository.java
@@ -1,5 +1,16 @@
 package com.enp.reservite.api.repository;
 
-public interface RoomRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.enp.reservite.api.entity.Room;
+
+public interface RoomRepository extends JpaRepository<Room,Long> {
+	
+	//IMPLEMENTAR CONSULTA POR PISO Y NUMERO DE HABITACION. LOS DATOS VIENEN DE SERVICE
+	@Query(value = "SELECT c.* FROM dbo_client c WHERE c.name LIKE %:nombre%", nativeQuery = true)
+	List<Room> findRoomByRoomNumber(Long floor,Long number);
 
 }
--- reservite.api/src/main/java/com/enp/reservite/api/service/RoomService.java
+++ reservite.api/src/main/java/com/enp/reservite/api/service/RoomService.java
@@ -1,5 +1,26 @@
 package com.enp.reservite.api.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.enp.reservite.api.entity.Room;
+import com.enp.reservite.api.repository.RoomRepository;
+
+@Service
 public class RoomService {
+	
+	@Autowired
+	RoomRepository roomRepository;
+
+	public Room save(Room room) {
+		return roomRepository.save(room);
+	}
+
+	public List<Room> findRoomByRoomNumber(String roomnumber) {
+		//IMPLEMENTAR CONFIGURACION DE PISO Y NUMERO DE HABITACION PARA CONSULTA. ENVIAR DATOS AL REPOSITORY
+		return roomRepository.findRoomByRoomNumber(1l,1l);
+	}
 
 }
