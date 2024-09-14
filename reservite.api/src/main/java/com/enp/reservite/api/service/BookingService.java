69af780cf4c564234fd87107b1dd423200e382e3
eneperezv <eneperezv@gmail.com>
2024-09-14 15:50:43 -0500

    Se incluyen nuevas dependencias. Se incluye actualizacion de entidad Room al realizar una reserva.
     Changes to be committed:
    	modified:   reservite.api/logs/reservite.api.log
    	modified:   reservite.api/pom.xml
    	modified:   reservite.api/src/main/java/com/enp/reservite/api/service/BookingService.java

--- reservite.api/src/main/java/com/enp/reservite/api/service/BookingService.java
+++ reservite.api/src/main/java/com/enp/reservite/api/service/BookingService.java
@@ -6,14 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.enp.reservite.api.entity.Booking;
+import com.enp.reservite.api.entity.Room;
 import com.enp.reservite.api.repository.BookingRepository;
+import com.enp.reservite.api.repository.RoomRepository;
 import com.enp.reservite.api.util.FunctionsUtil;
 
 @Service
 public class BookingService {
+
+	private final static Long ROOM_AVAILABLE = 1l;
+	private final static Long ROOM_OCCUPIED  = 0l;
 	
 	@Autowired
 	BookingRepository bookingRepository;
+	
+	@Autowired
+	RoomRepository roomRepository;
 	
 	FunctionsUtil func = new FunctionsUtil();
 
@@ -23,8 +31,15 @@ public class BookingService {
 		  .append(func.completaStringCeros(booking.getRoom().getRoomnumber(),"5"))
 		  .append(func.completaStringCeros(Long.toString(booking.getClient().getId()),"5"));
 		booking.setQrcode(func.retornaMD5(sb.toString()));
+		
+		Room room = roomRepository.findById(booking.getRoom().getId()).get();
+		room.setStatus(ROOM_OCCUPIED);
+		Room savedRoom = roomRepository.save(room);
+		
+		Booking savedBooking = bookingRepository.save(booking);
+		savedBooking.setRoom(savedRoom);
 
-		return bookingRepository.save(booking);
+		return savedBooking;
 	}
 
 	public List<Booking> findAll() {
