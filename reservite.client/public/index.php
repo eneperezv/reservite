f5ae876d25e4c3687a66922a391691ac857f4d51
eneperezv <eneperezv@gmail.com>
2024-09-16 22:27:31 -0500

    Se incluye documentacion inicial de las clases.
     Changes to be committed:
    	modified:   reservite.client/public/css/style.css
    	modified:   reservite.client/public/index.php
    	modified:   reservite.client/public/js/script.js

--- reservite.client/public/index.php
+++ reservite.client/public/index.php
@@ -1,4 +1,20 @@
 <?php
+/*
+ * @(#)index.php 1.0 15/09/2024
+ * 
+ * El código implementado en este formulario esta protegido
+ * bajo las leyes internacionales del Derecho de Autor, sin embargo
+ * se entrega bajo las condiciones de la General Public License (GNU GPLv3)
+ * descrita en https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+/**
+ * Entrada de la aplicación para inicializar.
+ *
+ * @author eliezer.navarro
+ * @version 1.0 | 15/09/2024
+ * @since 1.0
+ */
 
 // Cargar la configuración y las clases necesarias
 require_once '../config/config.php';
