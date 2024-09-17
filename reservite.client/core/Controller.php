5c5d2008b5dc9893d9b238b3be88ce49f247549c
eneperezv <eneperezv@gmail.com>
2024-09-16 22:24:53 -0500

    Se incluye documentacion inicial de clases.
     Changes to be committed:
    	modified:   reservite.client/config/config.php
    	modified:   reservite.client/core/App.php
    	modified:   reservite.client/core/Controller.php

--- reservite.client/core/Controller.php
+++ reservite.client/core/Controller.php
@@ -1,4 +1,20 @@
 <?php
+/*
+ * @(#)Controller.php 1.0 15/09/2024
+ * 
+ * El código implementado en este formulario esta protegido
+ * bajo las leyes internacionales del Derecho de Autor, sin embargo
+ * se entrega bajo las condiciones de la General Public License (GNU GPLv3)
+ * descrita en https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+/**
+ * Clase general Controller
+ *
+ * @author eliezer.navarro
+ * @version 1.0 | 15/09/2024
+ * @since 1.0
+ */
 class Controller{
     
     public function model($model){
