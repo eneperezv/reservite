f4b911433a1afc16b5764feafe6534f637aa0816
eneperezv <eneperezv@gmail.com>
2024-09-16 22:22:25 -0500

    Se incluye documentacion inicial de las clases.
     Changes to be committed:
    	modified:   reservite.client/app/views/booking.php
    	modified:   reservite.client/app/views/confirmation.php
    	modified:   reservite.client/app/views/index.php
    	modified:   reservite.client/app/views/login.php

--- reservite.client/app/views/login.php
+++ reservite.client/app/views/login.php
@@ -1,4 +1,22 @@
 <!DOCTYPE html>
+<?php
+/*
+ * @(#)login.php 1.0 15/09/2024
+ * 
+ * El código implementado en este formulario esta protegido
+ * bajo las leyes internacionales del Derecho de Autor, sin embargo
+ * se entrega bajo las condiciones de la General Public License (GNU GPLv3)
+ * descrita en https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+/**
+ * Clase Vista para gestion de autenticacion
+ *
+ * @author eliezer.navarro
+ * @version 1.0 | 15/09/2024
+ * @since 1.0
+ */
+?>
 <html>
 <head>
     <title>Login</title>
