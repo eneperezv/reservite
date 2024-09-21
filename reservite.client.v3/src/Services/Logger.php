211b59286e8e73476cb3fc729be223897206e1f3
eneperezv <eneperezv@gmail.com>
2024-09-20 23:19:01 -0500

    Se incluye documentacion inicial de la clase.
     Changes to be committed:
    	modified:   reservite.client.v3/src/Services/Logger.php

--- reservite.client.v3/src/Services/Logger.php
+++ reservite.client.v3/src/Services/Logger.php
@@ -1,4 +1,18 @@
 <?php
+/*
+ * @(#)Logger.php 1.0 19/09/2024
+ * 
+ * El código implementado en este formulario esta protegido
+ * bajo las leyes internacionales del Derecho de Autor, sin embargo
+ * se entrega bajo las condiciones de la General Public License (GNU GPLv3)
+ * descrita en https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+/**
+ * @author eliezer.navarro
+ * @version 1.0 | 19/09/2024
+ * @since 1.0
+ */ 
 
 namespace App\Services;
 
