083e1c3d21ffcbfbc47bbd220871a4e8bb4d08d8
eneperezv <eneperezv@gmail.com>
2024-09-22 13:14:51 -0500

    Se reorganiza implementacion de menu principal para que tome la opcion correspondiente.
     Changes to be committed:
    	modified:   reservite.api/logs/reservite.api.log
    	modified:   reservite.client.v3/src/Pages/main_menu.php

--- reservite.client.v3/src/Pages/main_menu.php
+++ reservite.client.v3/src/Pages/main_menu.php
@@ -1,4 +1,22 @@
-            <div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
+<?php
+$aMain = ''; 
+$aClient = ''; 
+$aHotel = ''; 
+$aRoom = ''; 
+$aBooking = ''; 
+if($_GET['page']=='main'){
+    $aMain = 'active';
+}elseif($_GET['page']=='clients'){
+    $aClient = 'active';
+}elseif($_GET['page']=='hotels'){
+    $aHotel = 'active';
+}elseif($_GET['page']=='rooms'){
+    $aRoom = 'active';
+}elseif($_GET['page']=='bookings'){
+    $aBooking = 'active';
+}
+?>
+<div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
                 <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
                     <div class="offcanvas-header">
                         <h5 class="offcanvas-title" id="sidebarMenuLabel">reservite.v3</h5>
@@ -7,7 +25,7 @@
                     <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
                         <ul class="nav flex-column">
                             <li class="nav-item">
-                                <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="dash.php?page=main">
+                                <a class="nav-link d-flex align-items-center gap-2 <?php print $aMain; ?>" aria-current="page" href="dash.php?page=main">
                                     <svg class="bi"><use xlink:href="#house-fill"/></svg>
                                     Dashboard
                                 </a>
@@ -16,19 +34,19 @@
                         <hr class="my-3">
                         <ul class="nav flex-column mb-auto">
                             <li class="nav-item">
-                                <a class="nav-link d-flex align-items-center gap-2" href="dash.php?page=clients">
+                                <a class="nav-link d-flex align-items-center gap-2 <?php print $aClient; ?>" href="dash.php?page=clients">
                                     <svg class="bi"><use xlink:href="#gear-wide-connected"/></svg>
                                     Clients
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link d-flex align-items-center gap-2" href="logout.php">
+                                <a class="nav-link d-flex align-items-center gap-2 <?php print $aHotel; ?>" href="logout.php">
                                     <svg class="bi"><use xlink:href="#door-closed"/></svg>
                                     Hotels
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link d-flex align-items-center gap-2" href="#">
+                                <a class="nav-link d-flex align-items-center gap-2 <?php print $aRoom; ?>" href="#">
                                     <svg class="bi"><use xlink:href="#file-earmark"/></svg>
                                     Rooms
                                 </a>
@@ -37,7 +55,7 @@
                         <hr class="my-3">
                         <ul class="nav flex-column mb-auto">
                             <li class="nav-item">
-                                <a class="nav-link d-flex align-items-center gap-2" href="#">
+                                <a class="nav-link d-flex align-items-center gap-2 <?php print $aBooking; ?>" href="#">
                                     <svg class="bi"><use xlink:href="#gear-wide-connected"/></svg>
                                     Booking
                                 </a>
