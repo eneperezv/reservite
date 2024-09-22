cf8a27e8ffff064a1c0af593b9fbd70d9a41fcff
eneperezv <eneperezv@gmail.com>
2024-09-21 23:42:53 -0500

    Se incluye archivo log de la api y archivo de respaldo de la base de datos.
     Changes to be committed:
    	modified:   reservite.api/logs/reservite.api.log
    	new file:   reservite.api/sql/reservite-2024-09-19.sql
    	modified:   reservite.api/sql/reservite.sql

--- reservite.api/sql/reservite-2024-09-19.sql
+++ reservite.api/sql/reservite-2024-09-19.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-09-2024 a las 06:42:13
+-- Tiempo de generación: 20-09-2024 a las 06:49:38
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
