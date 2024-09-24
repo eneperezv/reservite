d2eed28fc1c7f8827bc2b65c354aa541293d76ab
eneperezv <eneperezv@gmail.com>
2024-09-23 22:28:54 -0500

    Se incluye documentacion de la clase
     Changes to be committed:
    	modified:   reservite.client.v3/src/Services/ApiService.php

--- reservite.client.v3/src/Services/ApiService.php
+++ reservite.client.v3/src/Services/ApiService.php
@@ -1,5 +1,18 @@
 <?php
+/*
+ * @(#)ApiService.php 1.0 22/09/2024
+ * 
+ * El código implementado en este formulario esta protegido
+ * bajo las leyes internacionales del Derecho de Autor, sin embargo
+ * se entrega bajo las condiciones de la General Public License (GNU GPLv3)
+ * descrita en https://www.gnu.org/licenses/gpl-3.0.html
+ */
 
+/**
+ * @author eliezer.navarro
+ * @version 1.0 | 22/09/2024
+ * @since 1.0
+ */ 
 namespace App\Services;
 
 class ApiService
