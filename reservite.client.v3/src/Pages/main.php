8a120e6f0788536b55b81444036de1c080b2e619
eneperezv <eneperezv@gmail.com>
2024-09-25 21:26:44 -0500

    PENDIENTES
     Changes to be committed:
    	modified:   reservite.client.v3/src/Pages/main.php

--- reservite.client.v3/src/Pages/main.php
+++ reservite.client.v3/src/Pages/main.php
@@ -14,6 +14,7 @@
  * @since 1.0
  */ 
 ?>
+<?php /* PENDIENTE ELIMINAR DATOS QUE SE MUESTRAN Y CARGAR WIDGETS CON DATOS (CLIENTES,HABITACIONES DISPONIBLES,RESERVAS, ETC) */ ?>
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                     <h1 class="h2">Dashboard</h1>
