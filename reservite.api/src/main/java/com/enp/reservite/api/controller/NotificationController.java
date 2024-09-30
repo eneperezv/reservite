7eb90c20859f89b58e05bca94140b939e999977a
eneperezv <eneperezv@gmail.com>
2024-09-30 13:26:44 -0500

    Se incluyen nuevas clases Service y Controller para la gestion de notificaciones.
     Changes to be committed:
    	new file:   reservite.api/src/main/java/com/enp/reservite/api/controller/NotificationController.java
    	new file:   reservite.api/src/main/java/com/enp/reservite/api/service/NotificationService.java

---
+++ reservite.api/src/main/java/com/enp/reservite/api/controller/NotificationController.java
@@ -0,0 +1,44 @@
+package com.enp.reservite.api.controller;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.enp.reservite.api.entity.Booking;
+import com.enp.reservite.api.entity.ErrorDetails;
+import com.enp.reservite.api.entity.Notification;
+
+@RestController
+@RequestMapping("/api/v1/reservite")
+public class NotificationController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(NotificationController.class);
+	
+	@Autowired
+	NotificationService notificationService;
+	
+	@GetMapping("/notification")
+	public ResponseEntity<?> findBookings(){
+		List<Notification> lista = new ArrayList<Notification>();
+		try{
+			notificationService.findAll().forEach(lista::add);
+			if(lista.isEmpty()) {
+				ErrorDetails err = new ErrorDetails(new Date(),HttpStatus.OK.toString(),"NO CONTENT");
+				return new ResponseEntity<ErrorDetails>(err,HttpStatus.OK);
+			}
+			return new ResponseEntity<List<Notification>>(lista, HttpStatus.OK);
+		}catch(Exception e){
+			ErrorDetails err = new ErrorDetails(new Date(),HttpStatus.INTERNAL_SERVER_ERROR.toString(),"INTERNAL SERVER ERROR");
+			return new ResponseEntity<ErrorDetails>(err, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+}
---
+++ reservite.api/src/main/java/com/enp/reservite/api/service/NotificationService.java
@@ -0,0 +1,20 @@
+package com.enp.reservite.api.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.enp.reservite.api.entity.Notification;
+
+@Service
+public class NotificationService {
+	
+	@Autowired
+	NotificationRepository notificationRepository;
+	
+	public List<Notification> findAll() {
+		return notificationRepository.findAll();
+	}
+
+}
