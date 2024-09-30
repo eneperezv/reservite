073b48efcbb99591769605743593026503717936
eneperezv <eneperezv@gmail.com>
2024-09-29 23:05:57 -0500

    Se incluyen archivos de log y cambios en la clase Service
     Changes to be committed:
    	modified:   reservite.api/logs/reservite.api.log
    	deleted:    reservite.api/logs/reservite.api.log.2024-09-21.0.gz
    	new file:   reservite.api/logs/reservite.api.log.2024-09-28.0.gz
    	modified:   reservite.api/src/main/java/com/enp/reservite/api/service/DashboardService.java

--- reservite.api/src/main/java/com/enp/reservite/api/service/DashboardService.java
+++ reservite.api/src/main/java/com/enp/reservite/api/service/DashboardService.java
@@ -1,7 +1,5 @@
 package com.enp.reservite.api.service;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,38 +11,18 @@ public class DashboardService {
 	
 	@Autowired
 	DashboardRepository dashboardRepository;
+	
 	public Dashboard getDashboardStats() {
-        // Obtener los datos del repositorio
         Object[] result = dashboardRepository.getDashboardStats();
-
-        // Log de los resultados para depuración
-        System.out.println("Resultado crudo de la consulta: " + Arrays.toString(result));
-
-        // Verificar que el resultado es un array de arrays
         if (result != null && result.length > 0 && result[0] instanceof Object[]) {
-            Object[] row = (Object[]) result[0];  // Obtener la primera fila de resultados
-
-            // Convertir los resultados a Long manejando nulos
+            Object[] row = (Object[]) result[0];
             Long totalClientes = row[0] != null ? ((Number) row[0]).longValue() : 0L;
             Long habitacionesDisponibles = row[1] != null ? ((Number) row[1]).longValue() : 0L;
             Long totalReservas = row[2] != null ? ((Number) row[2]).longValue() : 0L;
-
             return new Dashboard(totalClientes, habitacionesDisponibles, totalReservas);
         } else {
-            // Manejo de error si los resultados son inesperados
             throw new IllegalStateException("Error al obtener datos del dashboard. Resultado inesperado.");
         }
     }
-	/*
-	public Dashboard getData() {
-		Object[] result = dashboardRepository.getDashboardStats();
-		
-		Long totalClientes = Long.parseLong(result[0].toString());// ((Number) result[0]).longValue();
-        Long habitacionesDisponibles = Long.parseLong(result[1].toString());// ((Number) result[1]).longValue();
-        Long totalReservas = Long.parseLong(result[2].toString());// ((Number) result[2]).longValue();
-        
-        
-		return new Dashboard(totalClientes,habitacionesDisponibles,totalReservas);
-	}
-	*/
+
 }
